d78b58179a722040289b843c1d1dba71dcbd3aff
OpenShift guest <aj222ah@student.lnu.se>
2013-10-30 12:45:32 -0400

    Småfix

---
+++ elements.js
@@ -0,0 +1,12 @@
+	document.createElement("header");
+	document.createElement("hgroup");
+	document.createElement("footer");
+	document.createElement("article");
+	document.createElement("main");
+	document.createElement("section");
+	document.createElement("nav");
+	document.createElement("aside");
+	document.createElement("address");
+	document.createElement("details");
+	document.createElement("figcaption");
+	document.createElement("figure");
